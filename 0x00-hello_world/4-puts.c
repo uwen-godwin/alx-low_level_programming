6c2dbe0cb4ae370799a629b8aafb730a9165aa9e
uwen-godwin <uwengodwin001@gmail.com>
2023-09-07 02:01:10 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - A program that print a line with puts function
+*Return: 0 (success)
+*/
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+Return(0);
+}
