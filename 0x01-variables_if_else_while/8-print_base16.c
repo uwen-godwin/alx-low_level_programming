d9a26abd554779f85bef19a6e045510f1ebbef73
uwen-godwin <uwengodwin001@gmail.com>
2023-09-09 06:19:12 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all the numbers of base
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+		for (i = 0; i < 10; i++)
+			putchar(i + '0');
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
