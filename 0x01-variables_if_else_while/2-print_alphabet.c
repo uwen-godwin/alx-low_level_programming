58a33e3af39dc3f601c00cc0b368c665c28923ca
uwen-godwin <uwengodwin001@gmail.com>
2023-09-08 21:21:35 -0700

    Numbers

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,14 @@
  * Return: Always 0
  *
  */
+
 int main(void)
 {
 	char i;
-
+	
 	for (i = 'a'; i <= 'z'; i++);
-	putchar(i);
+	
+		putchar(i);
 	putchar("\n");
-
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  Write a program that prints the alphabet
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'Z'; i++)
+		putchar(i);
+	
+	for (i = 'a'; i <= 'Z'; i++);
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
