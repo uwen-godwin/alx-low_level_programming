bedc3aca003ec1f16319b58c8c633880b80a468d
uwen-godwin <uwengodwin001@gmail.com>
2023-09-12 20:36:14 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,9 @@
 #include "main.h"
-
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print_times_table - printing my codes on a daily
+ * @n: The number to print
+ *
+ * Return: void
  */
 void print_times_table(int n)
 {
@@ -10,43 +11,41 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-				else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchat(' ');
-					_putchat(k >= 10 && k < 100)
-				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-                                        _putchar(' ');
-					_putchar(' ');
-                                        _putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar(( % 10) + '0')
-				}
-			}
-			_putchar('\n');
-		}
+	for (i = 0; i <= n; i++)
+	{
+	for (j = 0; j <= n; j++)
+	{
+	k = j * i;
+	if (j == 0)
+	{
+	_putchar(k + '0');
+	}
+	else if (k < 10 && j != 0)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(k + '0');
+	}
+	else if (k >= 10 && k < 100)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar((k / 10) + '0');
+	_putchar((k % 10) + '0');
+	}
+	else if (k >= 100)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar((k / 100) + '0');
+	_putchar(((k / 10) % 10) + '0');
+	_putchar((k % 10) + '0');
+	}
+	}
+	_putchar('\n');
+	}
 	}
 }
-
-
