e9f81f9576feb8a459e64a90fd1c8a4ab5461fd6
uwen-godwin <uwengodwin001@gmail.com>
2023-09-12 19:09:34 -0700

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * File: 101-natural
+ * Auth: Godwin uwenn
+ */
+int main(void)
+{
+	int, sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == || (i % 5 ) == 0)
+	}
+	printf("%d", sum)
+		return (0)
+}
