7b7d2a3d9ef78b7249bb31081c4e1ccefa8f61ff
uwen-godwin <uwengodwin001@gmail.com>
2023-09-14 06:35:58 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers 0 to 9
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	int pr;
+
+	for (pr = 48; pr <= 57; pr++)
+	{
+		_putchar(pr);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 int _isupper(int c);
+int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
