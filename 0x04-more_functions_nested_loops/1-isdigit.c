1db877db9bf873ac5c7ecd6490b7f3599f099a16
uwen-godwin <uwengodwin001@gmail.com>
2023-09-14 06:12:40 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit -  function that checks for a digit (0 through 9)
+ * @c: function parameter
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
