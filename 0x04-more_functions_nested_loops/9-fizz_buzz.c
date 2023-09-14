3b91b5502805256350e050dfa009833c3ebfde84
uwen-godwin <uwengodwin001@gmail.com>
2023-09-14 09:20:37 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print 1 to 100 and fibunacci
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FIZZBUZZ ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("FIZZ ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("BUZZ ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("BUZZ");
+	printf("\n");
+	return (0);
+}
