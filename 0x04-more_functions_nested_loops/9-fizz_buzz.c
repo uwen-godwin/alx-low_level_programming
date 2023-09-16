f49775069240055dec42fe899780111fc1e7d0aa
uwen-godwin <uwengodwin001@gmail.com>
2023-09-15 18:58:54 -0700

    The “Fizz-Buzz test” is an interview question designed to help filter out the 99.5% of programming job candidates who can’t seem to program their way out of a wet paper bag

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,8 +9,26 @@ int main(void)
 	int i;
 
 	for (i = 1; i < 100; i++)
-		if ((i % 3) == 0 && (i % 5) == 0)
+	{
+		if ((i % 5) == 0 && (i % 3) == 0)
 		{
-			printf("FizzBuzz ");
+		printf("FizzBuzz ");
 		}
+		else if (i % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", i);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
 
+	return (0);
+}
