88ebeefc719621bd3108c2b838ae98ed19cf81c4
uwen-godwin <uwengodwin001@gmail.com>
2023-09-21 12:10:11 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,8 +11,8 @@ int main(void)
 	/*
 	* Add your line of code here...
 	* Remember:
-	* - you are not allowed to use 'a'
-	* - you are not allowed to modify 'p'
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
 	* - only one statement
 	* - you are not allowed to code anything else than this line of code
 	*/
@@ -21,4 +21,3 @@ int main(void)
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
-
