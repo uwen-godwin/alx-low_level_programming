ada2fd2ebd652f77087415d4833aa720dbfe9bf6
uwen-godwin <uwengodwin001@gmail.com>
2023-09-21 20:18:24 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,92 +1,60 @@
 #include "main.h"
 #include <stdio.h>
-
-
 /**
-
- * print_buffer - prints buffer
-
- * @b: buffer
-
- * @size: size
-
+ * print_line - prints a.
+ * @c: buffer to print codes
+ * @s: bytes of buffering codes for me
+ * @l: line of codes for last
+ *
  * Return: void
-
  */
 
-void print_buffer(char *b, int size)
-
+void print_line(char *c, int s, int l)
 {
-
-        int o, j, i;
-
-
-        o = 0;
-
-
-        if (size <= 0)
-
-        {
-
-                printf("\n");
-
-                return;
-
-        }
-
-        while (o < size)
-
-        {
-
-                j = size - o < 10 ? size - o : 10;
-
-                printf("%08x: ", o);
-
-                for (i = 0; i < 10; i++)
-
-                {
-
-                        if (i < j)
-
-                                printf("%02x", *(b + o + i));
-
-                        else
-
-                                printf("  ");
-
-                        if (i % 2)
-
-                        {
-
-                                printf(" ");
-
-                        }
-
-                }
-
-                for (i = 0; i < j; i++)
-
-                {
-
-                        int c = *(b + o + i);
-
-
-                        if (c < 32 || c > 132)
-
-                        {
-
-                                c = '.';
-
-                        }
-
-                        printf("%c", c);
-
-                }
-
-                printf("\n");
-
-                o += 10;
-
-        }
-
+	int j = 0, k = 0;
+	while (j <= 9)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+		j++;
+	}
+	while (k <= s)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+		k++;
+	}
+}
+/**
+ * print_buffer - prints a buffering codes
+ * @b: buffe
+ * @size: size
+ *
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0;
+	while (i <= (size - 1) / 10 && size)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+			else
+			{
+				print_line(b, size % 10 - 1, i);
+				putchar('\n');
+				i++;
+			}
+			if (size == 0)
+				putchar('\n');
+		}
+	}
 }
