508f494e955a93dc049fb315f923d3a07609ecdd
uwen-godwin <uwengodwin001@gmail.com>
2023-09-21 18:53:05 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work #advanced

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
- * Returns: 0 upon successful completion.
+ *
+ * This function initializes variables and performs certain operations.
+ * It returns 0 upon successful completion.
+ *
+ * Return: 0 upon successful completion
  */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+		int n;
+		int a[5];
+		int *p;
 
-	a[2] = 1024;
-	p = &n;
-       /*
-	* write your line of code here...
-	* Remember:
-	* - you are not allowed to use 'a'
-	* - you are not allowed to modify 'p'
-	* - only one statement
-	* - you are not allowed to code anything else than this line of code
-	*/
+		a[2] = 1024;
+		p = &n;
+		/*
+		* Add your line of code here...
+		* Remember:
+		* - you are not allowed to use 'a'
+		* - you are not allowed to modify 'p'
+		* - only one statement
+		* - you are not allowed to code anything else than this line of code
+		*/
 
-	p[5] = 98;
+		*(p + 2) = 98;
 
-	/* ...so that this prints 98\n */
+		/* ...so that this prints 98\n */
 
-	printf("a[2] = %d\n", a[2]);
+		printf("a[2] = %d\n", a[2]);
 
-	return (0);
-}
+		return (0);
+
