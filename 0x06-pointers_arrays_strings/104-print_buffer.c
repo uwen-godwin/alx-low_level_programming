e2672adbe1067f88907c3fc54934eb14c5e2a620
uwen-godwin <uwengodwin001@gmail.com>
2023-09-22 08:11:02 -0700

    Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,26 +11,27 @@
 
 void print_line(char *c, int s, int l)
 {
-	int j = 0, k = 0;
-	while (j <= 9)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-		j++;
-	}
-	while (k <= s)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-		k++;
-	}
+int j = 0, k = 0;
+while (j <= 9)
+{
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
+else
+printf("  ");
+if (j % 2)
+putchar(' ');
+j++;
 }
+while (k <= s)
+{
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+putchar(c[l * 10 + k]);
+else
+putchar('.');
+k++;
+}
+}
+
 /**
  * print_buffer - prints a buffering codes
  * @b: buffe
@@ -40,21 +41,21 @@ void print_line(char *c, int s, int l)
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0;
-	while (i <= (size - 1) / 10 && size)
-	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-			else
-			{
-				print_line(b, size % 10 - 1, i);
-				putchar('\n');
-				i++;
-			}
-			if (size == 0)
-				putchar('\n');
-		}
-	}
+int i = 0;
+while (i <= (size - 1) / 10 && size)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+i++;
+}
+if (size == 0)
+putchar('\n');
 }
