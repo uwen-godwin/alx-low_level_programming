755c18a3a831e582bf75aba665a2158a3323b7b0
uwen-godwin <uwengodwin001@gmail.com>
2023-09-25 08:24:21 -0700

    A  function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x <	8; x++)
+	{
+		for(y = 0; y < 8; y++)
+		_putchar(a[x][y]);
+		_putchar('\n');
+	}
+}
