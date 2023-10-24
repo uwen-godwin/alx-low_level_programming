3f5f1c3d9955c1d675bb12ce8c5bd6d5300a0b0a
uwen-godwin <uwengodwin001@gmail.com>
2023-10-24 10:48:11 -0700

    updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,6 +22,7 @@ typedef struct list_s
 
 /*********** FUNCTIONS PROTOTYPES ***************/
 
+size_t list_len(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
 size_t print_list(const list_t *h);
 void free_list(list_t *head);
