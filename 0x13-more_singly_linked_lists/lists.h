3f56f2f5b385d75ac313092666cb84c56b31241c
uwen-godwin <uwengodwin001@gmail.com>
2023-10-24 11:10:55 -0700

    updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,5 +22,6 @@ typedef struct listint_s
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+int _putchar(char c);
 
 #endif
