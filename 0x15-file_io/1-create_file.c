abe15a79d2ce5572eb2375f420574a08ced316c2
uwen-godwin <uwengodwin001@gmail.com>
2023-10-30 17:49:09 -0700

    A function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,26 +9,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, bytes_written, text_length = 0;
-    mode_t mode = S_IRUSR | S_IWUSR;
+		int file_descriptor, bytes_written, text_length = 0;
+		mode_t mode = S_IRUSR | S_IWUSR;
 
-    if (filename == NULL)
-        return (-1);
+		if (filename == NULL)
+		return (-1);
 
-    file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
-    if (file_descriptor == -1)
-        return (-1);
+		file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+		if (file_descriptor == -1)
+		return (-1);
 
-    if (text_content != NULL) {
-        while (text_content[text_length])
-            text_length++;
-        bytes_written = write(file_descriptor, text_content, text_length);
-        if (bytes_written == -1) {
-            close(file_descriptor);
-            return (-1);
-        }
-    }
+		if (text_content != NULL)
+		{
+		while (text_content[text_length])
+		text_length++;
+		bytes_written = write(file_descriptor, text_content, text_length);
+		if (bytes_written == -1)
+		{
+		close(file_descriptor);
+		return (-1);
+		}
+		}
 
-    close(file_descriptor);
-    return (1);
+		close(file_descriptor);
+		return (1);
 }
