2b020b3794caf8490f7ac6757714cc17ec687d5e
uwen-godwin <uwengodwin001@gmail.com>
2023-12-12 08:32:52 -0800

    Giga Millions program

---
+++ 0x18-dynamic_libraries/allfun.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - add two integers
+ * @a: first integer to added
+ * @b: second integer to added
+ * Return: result of their summation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function to return the substracted value
+ * @a: first valued to be substracted
+ * @b: second value to be substracted
+ * Return: The result of their substraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - Multiplication of two integers
+ * @a: First value to be considered
+ * @b: second value to considered
+ * Return: The result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Division of two integer values
+ * @a: first value
+ * @b: second value
+ * Return: Result of the divition
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - Modulus of two integer values
+ * @a: first value
+ * @b: second values
+ * Return: The result of the mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
