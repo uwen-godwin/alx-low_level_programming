36874165678eef75ee0cea8f832ef09e2872f820
uwen-godwin <uwengodwin001@gmail.com>
2024-04-14 02:31:43 -0700

    search algorithms

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in a sorted array of integers
+ * using the Jump search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: Always EXIT_SUCCESS
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i = 0, small;
+	size_t jump = sqrt(size);
+
+	while ()
+}
